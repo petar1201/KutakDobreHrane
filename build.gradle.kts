29adc83db306630a97e4c731095c6edc7d5e5ab5
Petar Markovic <petar3747@gmail.com>
2024-06-15 03:33:40 +0200

    Setup Flyway

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.3.0"
 	id("io.spring.dependency-management") version "1.1.5"
+	id("org.flywaydb.flyway") version "9.16.3"
+
 }
 
 group = "com.etf.bg.ac.rs.mp200329"
@@ -28,8 +30,11 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-jdbc")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.flywaydb:flyway-core:10.15.0")
+	runtimeOnly("org.flywaydb:flyway-database-postgresql:10.15.0")
+	implementation("org.springframework.boot:spring-boot-starter-security")
 	compileOnly("org.projectlombok:lombok")
-	runtimeOnly("org.postgresql:postgresql")
+	runtimeOnly("org.postgresql:postgresql:42.7.0")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
@@ -37,4 +42,11 @@ dependencies {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+flyway {
+	url = "jdbc:postgresql://localhost:5430/kutak_dobre_hrane"
+	user = "postgres"
+	password = "pass123"
+	cleanDisabled = false
 }
